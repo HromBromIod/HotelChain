8b4bc23df995724b3d463e6d70f5d422cffd4464
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2024-10-15 19:29:30 +0300

    create web.api application, add Swagger and Serilog settings

---
+++ HotelChain.Service/IoC/ApplicationConfigurator.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelChain.Service.IoC;
+
+public static class ApplicationConfigurator
+{
+    public static void ConfigureServices(WebApplicationBuilder builder)
+    {
+        SwaggerConfigurator.ConfigureServices(builder.Services);
+        SerilogConfigurator.ConfigureServices(builder);
+    }
+
+    public static void ConfigureApplication(WebApplication app)
+    {
+        SwaggerConfigurator.ConfigureApplication(app);
+        SerilogConfigurator.ConfigureApplication(app);
+    }
+}
