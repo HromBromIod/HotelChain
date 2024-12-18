829a8fe4c4c43a2818c12b0dd10f254b9bbd3991
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2024-12-18 19:20:28 +0300

    users bl unit-tests

---
+++ HotelChain.BL.UnitTests/Helpers/MapperHelper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using HotelChain.BL.Mapper;
+
+namespace HotelChain.BL.UnitTests.Helpers;
+
+public static class MapperHelper
+{
+    static MapperHelper()
+    {
+        var config = new MapperConfiguration(x => x.AddProfile(typeof(UsersBLProfile)));
+        Mapper = new AutoMapper.Mapper(config);
+    }
+
+    public static IMapper Mapper { get; }
+}
---
+++ HotelChain.BL.UnitTests/Users/UsersProviderTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+using HotelChain.BL.UnitTests.Helpers;
+using HotelChain.BL.Users.Provider;
+using HotelChain.DataAccess.Entities;
+using HotelChain.Repository;
+using Moq;
+
+namespace HotelChain.BL.UnitTests.Users;
+
+public class UsersProviderTests
+{
+    [Test]
+    public void GetUsersTest()
+    {
+        Expression expression = null;
+        var repositoryMock = new Mock<IRepository<UserEntity>>();
+        repositoryMock.Setup(repository => repository.GetAll(It.IsAny<Expression<Func<UserEntity, bool>>>()))
+            .Callback((Expression<Func<UserEntity, bool>> x) => expression = x);
+        var usersProvider = new UsersProvider(repositoryMock.Object, MapperHelper.Mapper);
+        var result = usersProvider.GetUsers();
+        
+        repositoryMock.Verify(repository => repository
+            .GetAll(It.IsAny<Expression<Func<UserEntity, bool>>>()), Times.Once);
+    }
+}
