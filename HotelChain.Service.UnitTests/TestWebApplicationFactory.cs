79f9c840fe4df125f93eac69fa1e06a919047ad7
Hrom_Brom_Iod <timofeygolukovich@gmail.com>
2024-12-18 16:03:07 +0300

    authorization service unit-tests

---
+++ HotelChain.Service.UnitTests/TestWebApplicationFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace HotelChain.Service.UnitTests;
+
+public class TestWebApplicationFactory(Action<IServiceCollection>? overrideDependencies = null)
+    : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services => overrideDependencies?.Invoke(services));
+    }
+}
